c3ab57ca1388d39b251cf0e528291f56181f6a01
Dhruv <dap332@drexel.edu>
2018-09-17 16:54:56 -0400

    1.4 done and tested

---
+++ exercises/1.4.go
@@ -0,0 +1,62 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 10.
+//!+
+
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+    filenames := make(map[string][]string)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, filenames)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts, filenames)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\t%s\n", n, line, filenames[line])
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int, filenames map[string][]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+        line := input.Text()
+		counts[line]++
+        if !foundIn(f.Name(),  filenames[line]){
+            filenames[line] = append(filenames[line], f.Name())
+        }
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+func foundIn(filename string, list []string) bool {
+    for _, name := range list{
+        if filename ==  name {
+            return true
+        }
+    }
+    return false
+}
+
+//!-
