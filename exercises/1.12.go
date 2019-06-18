debf2938c7418968e1437ec852e55bd8391dcbe4
Dhruv <dap332@drexel.edu>
2019-06-18 15:02:40 -0400

    web server with lissajous and form parsing

---
+++ exercises/1.12.go
@@ -0,0 +1,104 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Run with "web" command-line argument for web server.
+// See page 13.
+//!+main
+
+// Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+        "strconv"
+)
+
+//!-main
+// Packages not needed by version in book.
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+//!+main
+
+var palette = []color.Color{
+	color.RGBA{0x00, 0x00, 0x00, 0xFF},
+	color.RGBA{0xFF, 0x00, 0x00, 0xFF},
+	color.RGBA{0xFF, 0xBF, 0x00, 0xFF},
+	color.RGBA{0xFF, 0xFF, 0x00, 0xFF},
+	color.RGBA{0x00, 0xFF, 0x00, 0xFF},
+	color.RGBA{0x00, 0xFF, 0xFF, 0xFF},
+	color.RGBA{0x40, 0x00, 0xFF, 0xFF},
+	color.RGBA{0xFF, 0x00, 0xFF, 0x00},
+}
+
+//const greenIndex = 1;
+
+func main() {
+	//!-main
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+        m := map[string]int{
+        "cycles": 5,
+        "size" : 100,
+        "nframes" : 64,
+        "delay" : 8 }
+ 
+        //!+http
+        handler := func(w http.ResponseWriter, r *http.Request) {
+                if err := r.ParseForm(); err != nil {
+                    log.Print(err)
+                }
+                for k, v := range r.Form {
+                    val, err := strconv.Atoi(v[0]);
+                        if err != nil{
+                            log.Print(err)
+                        }
+                    m[k] = val
+                }
+                lissajous(w, m)
+        }
+        http.HandleFunc("/", handler)
+        //!-http
+        log.Fatal(http.ListenAndServe("localhost:8000", nil))
+        return
+}
+
+func lissajous(out io.Writer, m map[string]int) {
+
+        cycles  := m["cycles"]  // number of complete x oscillator revolutions
+        res     := 0.001 // angular resolution
+        size    := m["size"]   // image canvas covers [-size..+size]
+        nframes := m["nframes"]    // number of animation frames
+        delay   := m["delay"]     // delay between frames in 10ms units
+	
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5),
+				(uint8)(i % 8))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+//!-main
